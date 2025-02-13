76dcab782b8c8ccbf495d6882eb3bcd65296f257
guichanah <dbs0467@gmail.com>
2025-02-13 12:17:57 +0900

    1

--- js/script.js
+++ js/script.js
@@ -160,7 +160,7 @@ function loadProducts(category, loadMore = false) {
     });
 
     currentCategory1 = category;
-    toggleLoadMoreButton();
+    //toggleLoadMoreButton();
 }
 
 // 탭 클릭 이벤트
@@ -175,3 +175,33 @@ document.querySelectorAll(".safetytab").forEach(tab => {
 
 // 페이지 로드 시 기본 데이터 표시
 loadProducts(currentCategory1);
+
+
+
+//모바일메뉴 및 서브메뉴
+var btns = document.querySelectorAll(".btn");
+
+       btns.forEach(function(btn){
+        btn.addEventListener("click",function(){
+            //다른 요소에 들어간 "active" 지우기
+            btns.forEach(function(re){
+                if(btn !== re)/*자기자신을 클릭 시 다시 닫히는 조건*/ 
+                re.parentElement.classList.remove("active");
+            });
+
+            btn.parentElement.classList.toggle("active");
+        });
+
+       })
+        console.log(btns);
+
+
+
+//메뉴영역 스크립트
+
+var mobilemenubtn = document.querySelector(".openmenu");
+var mobilemenu = document.querySelector("#mobilemenu");
+
+mobilemenubtn.addEventListener("click", function(){
+    mobilemenu.classList.toggle("active");
+});
